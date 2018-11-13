b83b534caa3a73a63ecc81b75132c8df41d5295f
Francisco Sanchez <fsancheztemprano@danielcastelao.org>
2018-11-13 11:46:01 +0100

    7

--- PROG_Boletin_8_7/PROG_Boletin_8_7.java
+++ PROG_Boletin_8_7/PROG_Boletin_8_7.java
@@ -12,18 +12,18 @@ import java.util.Scanner;
  * @author fsancheztemprano
  */
 public class PROG_Boletin_8_7 {
-    
+
     public static void main(String[] args) {
-       Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         float lado;
-        String label="";
-        do{
-        System.out.println(label+"Introduce el lado del cuadrado: ");
-        lado = scan.nextFloat();
-        label="El numero debe ser positvo \n";
-        }while(lado<0);
-        float area = lado*lado;
+        String label = "";
+        do {
+            System.out.println(label + "Introduce el lado del cuadrado: ");
+            lado = scan.nextFloat();
+            label = "El numero debe ser positvo \n";
+        } while (lado < 0);
+        float area = lado * lado;
         System.out.println("Area es : " + area);
     }
-    
+
 }

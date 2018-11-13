14f4ca6ded70c59ac6431dbb90e312aad5beea1e
Francisco Sanchez <fsancheztemprano@danielcastelao.org>
2018-11-13 11:25:03 +0100

    2

---
+++ PROG_Boletin_8_2/PROG_Boletin_8_2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PROG_Boletin_8_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author fsancheztemprano
+ */
+public class PROG_Boletin_8_2 {
+        public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        for(int i=0; i<5;i++){
+            
+            System.out.println(scan.nextInt());
+        }
+    }
+}

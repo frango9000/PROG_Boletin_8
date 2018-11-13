79132c0d7e258320b18dfdab16522368c83f2be4
Francisco Sanchez <fsancheztemprano@danielcastelao.org>
2018-11-13 11:48:44 +0100

    8

---
+++ PROG_Boletin_8_8/PROG_Boletin_8_8.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PROG_Boletin_8_8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author fsancheztemprano
+ */
+public class PROG_Boletin_8_8 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        float lado;
+        do {
+            String label = "";
+            do {
+                System.out.println(label + "Introduce el lado del cuadrado: ");
+                lado = scan.nextFloat();
+                label = "El numero debe ser positvo \n";
+            } while (lado < 0);
+            float area = lado * lado;
+            System.out.println("Area es : " + area);
+        } while (lado != 0);
+    }
+
+}
